72ab2a74cf3fedc5755653fa00989c74f5b1f6fb
formfcw <dev@formfcw.com>
2023-07-27 12:03:35 +0200

    refactor extensions

---
+++ shared/extensions.ts
@@ -0,0 +1,38 @@
+// Used for `/content` and `/src/display`
+// Add only extensions that are necessary for rendering the content
+
+import Document from "@tiptap/extension-document";
+import Text from "@tiptap/extension-text";
+import Paragraph from "@tiptap/extension-paragraph";
+import HardBreak from "@tiptap/extension-hard-break";
+import Heading from "@tiptap/extension-heading";
+import CodeBlock from "@tiptap/extension-code-block";
+import BulletList from "@tiptap/extension-bullet-list";
+import OrderedList from "@tiptap/extension-ordered-list";
+import ListItem from "@tiptap/extension-list-item";
+import Blockquote from "@tiptap/extension-blockquote";
+import HorizontalRule from "@tiptap/extension-horizontal-rule";
+import Link from "@tiptap/extension-link";
+import Bold from "@tiptap/extension-bold";
+import Italic from "@tiptap/extension-italic";
+import Strike from "@tiptap/extension-strike";
+import Code from "@tiptap/extension-code";
+
+export default [
+    Document,
+    Text,
+    Paragraph,
+    HardBreak,
+    Heading,
+    CodeBlock,
+    BulletList,
+    OrderedList,
+    ListItem,
+    Blockquote,
+    HorizontalRule,
+    Link,
+    Bold,
+    Italic,
+    Strike,
+    Code,
+];
