93f0927a4dea375c51e097217a395472b282a6fd
formfcw <dev@formfcw.com>
2024-03-29 10:52:28 +0100

    format

--- src/interface/tools/textAlign.ts
+++ src/interface/tools/textAlign.ts
@@ -4,15 +4,17 @@ import customMessages from "../i18n/custom-messages";
 import type { Editor } from "@tiptap/core";
 import type { Tool } from "../types";
 
-const textAlignOptions = ['left', 'center', 'right', 'justify'];
+const textAlignOptions = ["left", "center", "right", "justify"];
 
 export const textAlignTools = textAlignOptions.map((alignment) => ({
-  key: `align_${alignment}`,
-  name: (customMessages.tools as any)[`align_${alignment}`],
-  icon: `format_align_${alignment}`,
-  extension: [TextAlign],
-  action: (editor: Editor) => editor.chain().focus().setTextAlign(alignment).run(),
-  disabled: (editor: Editor) => !editor.can().chain().focus().setTextAlign(alignment).run(),
-  active: (editor: Editor) => editor.isActive('textStyle', { textAlign: alignment }),
+    key: `align_${alignment}`,
+    name: (customMessages.tools as any)[`align_${alignment}`],
+    icon: `format_align_${alignment}`,
+    extension: [TextAlign],
+    action: (editor: Editor) =>
+        editor.chain().focus().setTextAlign(alignment).run(),
+    disabled: (editor: Editor) =>
+        !editor.can().chain().focus().setTextAlign(alignment).run(),
+    active: (editor: Editor) =>
+        editor.isActive("textStyle", { textAlign: alignment }),
 })) as Tool[];
-
