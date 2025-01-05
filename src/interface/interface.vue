e82e4366cec25d5d3e67e3dca44f531c99fda7d1
formfcw <dev@formfcw.com>
2025-01-05 01:09:50 +0100

    format

--- src/interface/interface.vue
+++ src/interface/interface.vue
@@ -26,37 +26,42 @@
         <editor-content
             :editor="editor"
             :spellcheck="spellcheck ? 'true' : 'false'"
-            :class="{ [font]: true, [editorHeight]: true, 'single-line': singleLineMode }"
+            :class="{
+                [font]: true,
+                [editorHeight]: true,
+                'single-line': singleLineMode,
+            }"
             class="flexible-editor"
         />
     </div>
 </template>
 
-
-
 <script setup lang="ts">
     // Imports
-    import { ref, toRef, watch, provide, computed } from 'vue'
-    import { useEditor, EditorContent, type JSONContent } from '@tiptap/vue-3'
-    import Toolbar from './components/Toolbar.vue'
-    import Document from '@tiptap/extension-document'
-    import Text from '@tiptap/extension-text'
-    import Paragraph from '@tiptap/extension-paragraph'
-    import Placeholder from '@tiptap/extension-placeholder'
-    import Dropcursor from '@tiptap/extension-dropcursor'
-    import Gapcursor from '@tiptap/extension-gapcursor'
-    import RelationBlock from "./tools/relation-block/node-extension"
-    import RelationInlineBlock from "./tools/relation-inline-block/node-extension"
-    import RelationMark from "./tools/relation-mark/node-extension"
-    import { toolsExtensions, interfaceOptionsDefault, selectedTools } from './tools'
-    import { useSyncRelationNodes } from "./composables/use-sync-relation-nodes"
-    import { useRelationReference } from './composables/use-relation-reference'
+    import { ref, toRef, watch, provide, computed } from "vue";
+    import { useEditor, EditorContent, type JSONContent } from "@tiptap/vue-3";
+    import Toolbar from "./components/Toolbar.vue";
+    import Document from "@tiptap/extension-document";
+    import Text from "@tiptap/extension-text";
+    import Paragraph from "@tiptap/extension-paragraph";
+    import Placeholder from "@tiptap/extension-placeholder";
+    import Dropcursor from "@tiptap/extension-dropcursor";
+    import Gapcursor from "@tiptap/extension-gapcursor";
+    import RelationBlock from "./tools/relation-block/node-extension";
+    import RelationInlineBlock from "./tools/relation-inline-block/node-extension";
+    import RelationMark from "./tools/relation-mark/node-extension";
+    import {
+        toolsExtensions,
+        interfaceOptionsDefault,
+        selectedTools,
+    } from "./tools";
+    import { useSyncRelationNodes } from "./composables/use-sync-relation-nodes";
+    import { useRelationReference } from "./composables/use-relation-reference";
     // import { useM2aStore } from './composables/use-m2a-store'
-    import { useI18n } from "vue-i18n"
-    import { useI18nFallback } from './composables/use-i18n-fallback'
+    import { useI18n } from "vue-i18n";
+    import { useI18nFallback } from "./composables/use-i18n-fallback";
     import type { Collection } from "./directus-core/types/collections";
-    import type { ToolbarMode, RelationReferenceAttributes } from './types'
-
+    import type { ToolbarMode, RelationReferenceAttributes } from "./types";
 
     // Props
     interface Props {
@@ -73,7 +78,10 @@
         displayFormat: boolean;
         font: string;
         spellcheck: boolean;
-        editorHeight: "height-fixed" | "height-grow" | "height-grow-till-overflow";
+        editorHeight:
+            | "height-fixed"
+            | "height-grow"
+            | "height-grow-till-overflow";
         field: string | null;
         collection: string | null;
         primaryKey: string | number | null;
@@ -82,37 +90,35 @@
         value: null,
         disabled: false,
         m2aField: null,
-        placeholder: '',
+        placeholder: "",
         inputMode: "multi",
         tools: () => interfaceOptionsDefault,
         toolbarMode: "static",
         displayFormat: false,
         font: "sans-serif",
         spellcheck: false,
-        editorHeight: 'height-grow-till-overflow',
+        editorHeight: "height-grow-till-overflow",
         field: null,
         collection: null,
         primaryKey: null,
     });
 
-
     // I18n
     const { t } = useI18nFallback(useI18n());
 
-
     // Emits
-    const emit = defineEmits(['input', 'setFieldValue']);
-
+    const emit = defineEmits(["input", "setFieldValue"]);
 
     // Input Mode
     const singleLineMode = computed(() => props.inputMode == "single");
-
 
     // TipTap Editor Setup
     const editor = useEditor({
         content: props.value,
         extensions: [
-            Document.extend(singleLineMode.value ? { content: "(text|singleline)*" } : {}),
+            Document.extend(
+                singleLineMode.value ? { content: "(text|singleline)*" } : {}
+            ),
             Text,
             Paragraph,
             Placeholder.configure({ placeholder: props.placeholder }),
@@ -121,7 +127,7 @@
             RelationBlock,
             RelationInlineBlock,
             RelationMark,
-            ...toolsExtensions(props.tools)
+            ...toolsExtensions(props.tools),
         ],
         onCreate() {
             // called twice to reset the items even if props.value (below) is empty
@@ -131,58 +137,69 @@
             syncRelationNodes();
 
             const editorValue = await editor.getJSON();
-            const emptyJSON = { "type": "doc", "content": [{ "type": "paragraph" }] };
-            const isEmpty = JSON.stringify(editorValue) === JSON.stringify(emptyJSON);
+            const emptyJSON = { type: "doc", content: [{ type: "paragraph" }] };
+            const isEmpty =
+                JSON.stringify(editorValue) === JSON.stringify(emptyJSON);
 
             if (isEmpty) editor.commands.setContent(null, false);
 
-            emit('input', isEmpty ? null : editorValue);
+            emit("input", isEmpty ? null : editorValue);
         },
     });
-    watch(() => props.value, (value) => {
-        const isSame = JSON.stringify(editor.value!.getJSON()) === JSON.stringify(value)
-        if (isSame) return;
-        editor.value!.commands.setContent(value, false);
-        resetRelationNodes();
-    });
+    watch(
+        () => props.value,
+        (value) => {
+            const isSame =
+                JSON.stringify(editor.value!.getJSON()) ===
+                JSON.stringify(value);
+            if (isSame) return;
+            editor.value!.commands.setContent(value, false);
+            resetRelationNodes();
+        }
+    );
     // The following lines are causing the update event to fire on editor creation, which leads to issues. Found out that directus disables the field globally, so this is not needed!
     // watch(() => props.disabled, (disabled) =>
     //     editor.value!.setEditable(!disabled)
     // );
 
-
     // Fallback function
-    let resetRelationNodes = () => { };
-    let syncRelationNodes = () => { };
-
+    let resetRelationNodes = () => {};
+    let syncRelationNodes = () => {};
 
     // Fullscreen mode
     const fullscreen = ref(false);
-    provide('fullscreen', fullscreen);
-
+    provide("fullscreen", fullscreen);
 
     // Errors
     const errors = ref<string[]>([]);
 
-
     if (props.m2aField) {
         const m2aRelation = useRelationReference({
-            m2aField: toRef(props, 'm2aField'),
-            editorField: toRef(props, 'field'),
-            itemCollection: toRef(props, 'collection'),
-            itemPrimaryKey: toRef(props, 'primaryKey'),
-            updateM2aField: (value) => emit("setFieldValue", { field: props.m2aField, value }),
-            relationBlocks: toRef(props, 'relationBlocks'),
-            relationInlineBlocks: toRef(props, 'relationInlineBlocks'),
-            relationMarks: toRef(props, 'relationMarks'),
+            m2aField: toRef(props, "m2aField"),
+            editorField: toRef(props, "field"),
+            itemCollection: toRef(props, "collection"),
+            itemPrimaryKey: toRef(props, "primaryKey"),
+            updateM2aField: (value) =>
+                emit("setFieldValue", { field: props.m2aField, value }),
+            relationBlocks: toRef(props, "relationBlocks"),
+            relationInlineBlocks: toRef(props, "relationInlineBlocks"),
+            relationMarks: toRef(props, "relationMarks"),
         } as RelationReferenceAttributes);
 
-        provide('m2aRelation', m2aRelation);
+        provide("m2aRelation", m2aRelation);
 
         errors.value = m2aRelation.errors.value;
 
         // sync relation nodes with M2A relation and M2A store
-        const { initFetchedItems, syncRelationNodes: _syncRelationNodes, resetRelationNodes: _resetRelationNodes } = useSyncRelationNodes({ m2aRelation, editor, editorField: props.field });
+        const {
+            initFetchedItems,
+            syncRelationNodes: _syncRelationNodes,
+            resetRelationNodes: _resetRelationNodes,
+        } = useSyncRelationNodes({
+            m2aRelation,
+            editor,
+            editorField: props.field,
+        });
         resetRelationNodes = _resetRelationNodes;
         syncRelationNodes = _syncRelationNodes;
 
@@ -190,19 +207,24 @@
     }
 </script>
 
-
-
 <style scoped>
-    .errors>div:not(:first-child) {
+    .errors > div:not(:first-child) {
         margin-top: 8px;
     }
 
     /* Field */
     .flexible-editor-wrapper {
         color: var(--theme--form--field--input--foreground, var(--foreground));
-        background-color: var(--theme--form--field--input--background, var(--background-page));
-        border: var(--theme--border-width, var(--border-width)) solid var(--theme--form--field--input--border-color, var(--border-normal));
-        border-radius: var(--v-input-border-radius, var(--theme--border-radius));
+        background-color: var(
+            --theme--form--field--input--background,
+            var(--background-page)
+        );
+        border: var(--theme--border-width, var(--border-width)) solid
+            var(--theme--form--field--input--border-color, var(--border-normal));
+        border-radius: var(
+            --v-input-border-radius,
+            var(--theme--border-radius)
+        );
     }
 
     .flexible-editor-wrapper:not(.toolbar-floating) {
@@ -210,18 +232,36 @@
     }
 
     .flexible-editor-wrapper:hover {
-        border-color: var(--theme--form--field--input--border-color-hover, var(--border-normal-alt));
+        border-color: var(
+            --theme--form--field--input--border-color-hover,
+            var(--border-normal-alt)
+        );
     }
 
     .flexible-editor-wrapper:focus-within {
-        border-color: var(--theme--form--field--input--border-color-focus, var(--primary));
-        box-shadow: var(--theme--form--field--input--box-shadow-focus, 0 0 16px -8px var(--v-input-box-shadow-color-focus));
+        border-color: var(
+            --theme--form--field--input--border-color-focus,
+            var(--primary)
+        );
+        box-shadow: var(
+            --theme--form--field--input--box-shadow-focus,
+            0 0 16px -8px var(--v-input-box-shadow-color-focus)
+        );
     }
 
     .disabled {
-        color: var(--theme--form--field--input--foreground-subdued, var(--foreground-subdued));
-        background-color: var(--theme--form--field--input--background-subdued, var(--background-subdued));
-        border-color: var(--theme--form--field--input--border-color, var(--border-normal));
+        color: var(
+            --theme--form--field--input--foreground-subdued,
+            var(--foreground-subdued)
+        );
+        background-color: var(
+            --theme--form--field--input--background-subdued,
+            var(--background-subdued)
+        );
+        border-color: var(
+            --theme--form--field--input--border-color,
+            var(--border-normal)
+        );
         pointer-events: none;
     }
 
@@ -255,7 +295,10 @@
 
     /* Fonts */
     .monospace {
-        font-family: var(--theme--font-family-monospace, var(--family-monospace));
+        font-family: var(
+            --theme--font-family-monospace,
+            var(--family-monospace)
+        );
     }
 
     .serif {
@@ -263,18 +306,33 @@
     }
 
     .sans-serif {
-        font-family: var(--theme--font-family-sans-serif, var(--family-sans-serif));
+        font-family: var(
+            --theme--font-family-sans-serif,
+            var(--family-sans-serif)
+        );
     }
 
     /* Editor */
     .flexible-editor-wrapper {
         --editor-lineheight: 1.6;
-        --editor-input-padding: var(--theme--form--field--input--padding, var(--input-padding));
+        --editor-input-padding: var(
+            --theme--form--field--input--padding,
+            var(--input-padding)
+        );
         /* --editor-height = --editor-lineheight * 7 */
-        --editor-height: calc(11.2em + var(--editor-input-padding) + var(--editor-input-padding));
+        --editor-height: calc(
+            11.2em + var(--editor-input-padding) + var(--editor-input-padding)
+        );
         /* --editor-height = --editor-lineheight * 1 */
-        --editor-height-single-line: calc(1.6em + var(--editor-input-padding) + var(--editor-input-padding));
-        --editor-overflow-height: calc(100vh - var(--header-bar-height) - var(--header-bar-height) - var(--theme--form--row-gap, var(--form-vertical-gap)) - var(--theme--form--row-gap, var(--form-vertical-gap)) - var(--editor-input-padding) - var(--editor-input-padding));
+        --editor-height-single-line: calc(
+            1.6em + var(--editor-input-padding) + var(--editor-input-padding)
+        );
+        --editor-overflow-height: calc(
+            100vh - var(--header-bar-height) - var(--header-bar-height) -
+                var(--theme--form--row-gap, var(--form-vertical-gap)) -
+                var(--theme--form--row-gap, var(--form-vertical-gap)) -
+                var(--editor-input-padding) - var(--editor-input-padding)
+        );
     }
 
     .flexible-editor {
@@ -288,7 +346,10 @@
     /* Editor Size */
     .flexible-editor :deep(.ProseMirror) {
         line-height: var(--editor-lineheight);
-        padding: var(--theme--form--field--input--padding, var(--input-padding));
+        padding: var(
+            --theme--form--field--input--padding,
+            var(--input-padding)
+        );
         overflow: auto;
     }
 
@@ -301,10 +362,10 @@
         white-space: nowrap;
         -ms-overflow-style: none;
         scrollbar-width: none;
-
     }
 
-    .flexible-editor.height-fixed.single-line :deep(.ProseMirror::-webkit-scrollbar) {
+    .flexible-editor.height-fixed.single-line
+        :deep(.ProseMirror::-webkit-scrollbar) {
         display: none;
     }
 
@@ -393,8 +454,12 @@
     }
 
     .flexible-editor :deep(blockquote) {
-        border-left: var(--theme--border-width, var(--border-width)) solid var(--theme--border-color, var(--border-normal));
-        padding-left: var(--theme--form--field--input--padding, var(--input-padding));
+        border-left: var(--theme--border-width, var(--border-width)) solid
+            var(--theme--border-color, var(--border-normal));
+        padding-left: var(
+            --theme--form--field--input--padding,
+            var(--input-padding)
+        );
     }
 
     .flexible-editor :deep(strong) {
@@ -415,7 +480,10 @@
     }
 
     .flexible-editor :deep(code) {
-        background-color: var(--theme--background-normal, var(--background-normal));
+        background-color: var(
+            --theme--background-normal,
+            var(--background-normal)
+        );
         padding: 2px 4px;
         border-radius: var(--theme--border-radius, var(--border-radius));
     }
@@ -427,8 +495,14 @@
     }
 
     .flexible-editor :deep(pre) {
-        background-color: var(--theme--background-normal, var(--background-normal));
-        padding: var(--theme--form--field--input--padding, var(--input-padding));
+        background-color: var(
+            --theme--background-normal,
+            var(--background-normal)
+        );
+        padding: var(
+            --theme--form--field--input--padding,
+            var(--input-padding)
+        );
         border-radius: var(--theme--border-radius, var(--border-radius));
     }
 
@@ -438,7 +512,10 @@
 
     .flexible-editor :deep(ol),
     .flexible-editor :deep(ul) {
-        padding-left: var(--theme--form--field--input--padding, var(--input-padding));
+        padding-left: var(
+            --theme--form--field--input--padding,
+            var(--input-padding)
+        );
     }
 
     .flexible-editor :deep(table) {
@@ -469,7 +546,10 @@
 
     /* Placeholder */
     .flexible-editor :deep(p.is-editor-empty:first-child::before) {
-        color: var(--theme--foreground-subdued, var(--v-input-placeholder-color));
+        color: var(
+            --theme--foreground-subdued,
+            var(--v-input-placeholder-color)
+        );
         content: attr(data-placeholder);
         float: left;
         height: 0;
@@ -498,7 +578,8 @@
         border-left: 1px solid black;
     }
 
-    .flexible-editor :deep(.ProseMirror .ProseMirror-gapcursor:first-child:after) {
+    .flexible-editor
+        :deep(.ProseMirror .ProseMirror-gapcursor:first-child:after) {
         right: auto;
         left: 0;
         bottom: auto;
@@ -511,17 +592,18 @@
         margin-top: 0;
     }
 
-    .flexible-editor :deep(.ProseMirror .ProseMirror-gapcursor:last-child:after) {
+    .flexible-editor
+        :deep(.ProseMirror .ProseMirror-gapcursor:last-child:after) {
         bottom: 0;
     }
 </style>
 
-
-
 <style>
-
     /* not scoped */
     .prosemirror-dropcursor-block {
-        background-color: var(--theme--border-color, var(--border-normal)) !important;
+        background-color: var(
+            --theme--border-color,
+            var(--border-normal)
+        ) !important;
     }
 </style>
